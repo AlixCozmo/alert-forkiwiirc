5b70431dfc24124e4d1c34d9691a74cf2cf1b42d
DavidByggerBilar <66059104+DavidByggerBilar@users.noreply.github.com>
2023-01-20 14:27:06 +0100

    Add files via upload

--- alert.js
+++ alert.js
@@ -1,18 +1,24 @@
-
+var lasttime;
 
 function GrabMessage() {
     let messagestring;
+    let messagetime;
     console.log("GrabMessage Started!");
     for (let messageloop = 0; messageloop < 10; messageloop++) {
         console.log("messageloop: " + messageloop);
         console.log("messageloop+240: " + messageloop+240);
         messagestring = window.kiwi.state.getBufferByName(1, "#ratchat").messagesObj.messages[messageloop+240].message;
+        messagetime = window.kiwi.state.getBufferByName(1, "#ratchat").messagesObj.messages[messageloop+240].time;
         messagestring = messagestring.toLower();
-        if (messagestring.includes("ratsignal")) {
+        if (messagestring.includes("ratsignal" && lasttime != messagetime)) {
+            lastrat = messagestring;
             console.log("RAT!");
+            console.log("messageloop: " + messageloop);
         }
-        if (messagestring.includes("hatsignal")) {
+        if (messagestring.includes("hatsignal" && lasttime != messagetime)) {
+            lasthat = messagestring;
             console.log("HAT!");
+            console.log("messageloop: " + messageloop);
         }
 
         }
