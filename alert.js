c49a8dc4da8cc92fb2959c09bd758383479bfc8a
DavidByggerBilar <66059104+DavidByggerBilar@users.noreply.github.com>
2023-01-20 13:55:45 +0100

    Add files via upload

--- alert.js
+++ alert.js
@@ -1,11 +1,21 @@
 var messagestring // used to hold the message
 
 function GrabMessage() {
-messagestring = window.kiwi.state.getBufferByName(1, "#fuelrats").messagesObj.messages[249].message;
+    console.log("GrabMessage Started!");
+    for (let messageloop = 0; messageloop < 10; messageloop++) {
+        console.log("messageloop: " + messageloop);
+        console.log("messageloop+240: " + messageloop+240);
+        messagestring = window.kiwi.state.getBufferByName(1, "#ratchat").messagesObj.messages[messageloop+240].message;
+        messagestring = messagestring.toLower();
+        if (messagestring.includes("ratsignal")) {
+            console.log("RAT!");
+        }
+        if (messagestring.includes("hatsignal")) {
+            console.log("HAT!");
+        }
 
-messagestring = messagestring.toLower();
-if (messagestring.includes("ratsignal")) {
-    console.log("RAT!");
+        }
+
 }
 
-}
+setInterval(GrabMessage, 5000);
