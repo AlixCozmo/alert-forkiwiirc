12ebe3bda844a9da66246350416217ff8f509d4d
DavidByggerBilar <66059104+DavidByggerBilar@users.noreply.github.com>
2023-01-20 15:24:29 +0100

    Add files via upload

--- alert.js
+++ alert.js
@@ -1,22 +1,39 @@
 var lasttime;
 
 function GrabMessage() {
-    let messagestring;
-    let messagetime;
+    let messagestringfuel;
+    let messagestringchat;
+    let messagetimechat;
+    let messagetimefuel;
     console.log("GrabMessage Started!");
     for (let messageloop = 0; messageloop < 10; messageloop++) {
         console.log("messageloop: " + messageloop);
         console.log("messageloop+240: " + messageloop+240);
-        messagestring = window.kiwi.state.getBufferByName(1, "#ratchat").messagesObj.messages[messageloop+240].message;
-        messagetime = window.kiwi.state.getBufferByName(1, "#ratchat").messagesObj.messages[messageloop+240].time;
-        messagestring = messagestring.toLower();
-        if (messagestring.includes("ratsignal" && lasttime < messagetime)) {
-            lastrat = messagestring;
+        messagestringfuel = window.kiwi.state.getBufferByName(1, "#fuelrats").messagesObj.messages[messageloop+240].message;
+        messagetimefuel = window.kiwi.state.getBufferByName(1, "#fuelrats").messagesObj.messages[messageloop+240].time;
+        messagestringchat = window.kiwi.state.getBufferByName(1, "#ratchat").messagesObj.messages[messageloop+240].message;
+        messagetimechat = window.kiwi.state.getBufferByName(1, "#ratchat").messagesObj.messages[messageloop+240].time;
+        messagestringchat = messagestring.toLower();
+        messagestringfuel = messagestring.toLower();
+        // RATCHAT
+        if (messagestring.includes("ratsignal" && lasttime < messagetimechat)) {
+            lastrat = messagestringchat;
             console.log("RAT!");
             console.log("messageloop: " + messageloop);
         }
-        if (messagestring.includes("hatsignal" && lasttime < messagetime)) {
-            lasthat = messagestring;
+        if (messagestring.includes("hatsignal" && lasttime < messagetimechat)) {
+            lasthat = messagestringchat;
+            console.log("HAT!");
+            console.log("messageloop: " + messageloop);
+        }
+        // FUELRAT
+        if (messagestring.includes("ratsignal" && lasttime < messagetimefuel)) {
+            lastrat = messagestringfuel;
+            console.log("RAT!");
+            console.log("messageloop: " + messageloop);
+        }
+        if (messagestring.includes("hatsignal" && lasttime < messagetimefuel)) {
+            lasthat = messagestringfuel;
             console.log("HAT!");
             console.log("messageloop: " + messageloop);
         }
